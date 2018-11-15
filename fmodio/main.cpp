65256b83b47bb474411918b6384a227c85bdaa31
Jake <jake.trefethen@gmail.com>
2018-11-14 22:50:57 -0800

    initial

---
+++ fmodio/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  fmodio
+//
+//  Created by Jake Trefethen on 11/14/18.
+//  Copyright © 2018 Jake Trefethen. All rights reserved.
+//
+
+#include <iostream>
+#include "fmod_studio.hpp"
+#include "fmod.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
