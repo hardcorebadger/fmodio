432ad36e97fe55d59d812fc7b3fc41c49f47eebc
Jake <jake.trefethen@gmail.com>
2018-11-14 23:33:55 -0800

    bootstrap

---
+++ fmodio/AudioEngine.h
@@ -0,0 +1,78 @@
+//
+//  AudioEngine.h
+//  fmodio
+//
+//  Created by Jake Trefethen on 11/14/18.
+//  Copyright © 2018 Jake Trefethen. All rights reserved.
+//
+
+#ifndef AudioEngine_h
+#define AudioEngine_h
+
+#include "fmod_studio.hpp"
+#include "fmod.hpp"
+#include <string>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+struct Vector3 {
+    float x;
+    float y;
+    float z;
+};
+
+struct Implementation {
+    Implementation();
+    ~Implementation();
+    
+    void Update();
+    
+    FMOD::Studio::System* mpStudioSystem;
+    FMOD::System* mpSystem;
+    
+    int mnNextChannelId;
+    
+    typedef map<string, FMOD::Sound*> SoundMap;
+    typedef map<int, FMOD::Channel*> ChannelMap;
+    typedef map<string, FMOD::Studio::EventInstance*> EventMap;
+    typedef map<string, FMOD::Studio::Bank*> BankMap;
+    
+    BankMap mBanks;
+    EventMap mEvents;
+    SoundMap mSounds;
+    ChannelMap mChannels;
+};
+
+class CAudioEngine {
+public:
+    static void Init();
+    static void Update();
+    static void Shutdown();
+    static int ErrorCheck(FMOD_RESULT result);
+    
+    void LoadBank(const string& strBankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags);
+    void LoadEvent(const string& strEventName);
+    void LoadSound(const string& strSoundName, bool b3d = true, bool bLooping = false, bool bStream = false);
+    void UnLoadSound(const string& strSoundName);
+    void Set3dListenerAndOrientation(const Vector3& vPos = Vector3{ 0, 0, 0 }, float fVolumedB = 0.0f);
+    int PlaySound(const string& strSoundName, const Vector3& vPos = Vector3{ 0, 0, 0 }, float fVolumedB = 0.0f);
+    void PlayEvent(const string& strEventName);
+    void StopChannel(int nChannelId);
+    void StopEvent(const string& strEventName, bool bImmediate = false);
+    void GeteventParameter(const string& strEventName, const string& strEventParameter, float* parameter);
+    void SetEventParameter(const string& strEventName, const string& strParameterName, float fValue);
+    void StopAllChannels();
+    void SetChannel3dPosition(int nChannelId, const Vector3& vPosition);
+    void SetChannelVolume(int nChannelId, float fVolumedB);
+    bool IsPlaying(int nChannelId) const;
+    bool IsEventPlaying(const string& strEventName) const;
+    float dBToVolume(float db);
+    float VolumeTodB(float volume);
+    FMOD_VECTOR VectorToFmod(const Vector3& vPosition);
+};
+
+#endif /* AudioEngine_h */
